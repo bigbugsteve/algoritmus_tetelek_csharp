2f19f847419e78c1a1fba836c3b199e0f61b8cb6
gaba1 <bruxblank@gmail.com>
2022-03-14 20:53:30 +0100

    kiveseztuk

--- algoritmus_tetelek_csharp/Program2.cs
+++ algoritmus_tetelek_csharp/Program2.cs
@@ -5,14 +5,21 @@ namespace Proj_02
 {
     class Log
     {
-        public static int BinarisKeres(int[] tomb, int keresettertek)
+        public static int  BinarisKeres(int[] tomb, int keresettertek)
         {
             int eleje = 0;
-            int vege = tomb.Length;
+            int vege = tomb.Length; // 13
+
             while (eleje <= vege)
             {
-                int i = (eleje + vege) / 2;
+                // 13 hosszú a tömb
+                // 
+
+                int i = (eleje + vege) / 2; // 7
+
+                //if(i == vege) return -1;
                 if (tomb[i] == keresettertek) return i;
+
                 else if (tomb[i] < keresettertek)
                 {
                     eleje = i + 1;
@@ -22,6 +29,8 @@ namespace Proj_02
                     vege = i - 1;
                 }
             }
+            Console.WriteLine("eleje értéke a while ciklus után: {0}", eleje);
+           
             return -1;
         }
 
@@ -43,23 +52,23 @@ namespace Proj_02
             return BinarisKeresRekurziv(tomb, keresettertek, mid + 1, vege);
         }
 
-        public static void Kiir(int param1, string param2)
-        {
-            Console.WriteLine("Parameter 1: {0}, Parameter 2: {1}", param1, param2);
-        }
-
         static void Main(string[] args)
         {
+
+
             var tomb = new int[] { 0, 0, 1, 2, 2, 2, 3, 1, 4, 5, 6, 8, 9 };
 
-            Kiir(2, "2");
+            var index = BinarisKeres(tomb, 8);  // 4
+            int index2 = BinarisKeresRekurziv(tomb, 8, 0, tomb.Length);
 
-            int index = BinarisKeres(tomb, 8);
-            int index2 = BinarisKeresRekurziv(tomb, 1, 0, tomb.Length);
-
-            //int index = Array.BinarySearch(tomb, 8);
-            Console.WriteLine("A nyolcas indexe: {0}", index);
-            Console.WriteLine("Az egyes indexe: {0}", index2);
+            if (index == -1)
+            {
+                Console.WriteLine("A keresett szám nincs a tömbben");
+            } else
+            {
+                Console.WriteLine("A nyolcas indexe: {0}", index);
+                Console.WriteLine("Az egyes indexe: {0}", index2);
+            }
 
             Console.ReadKey();
         }
