3b61c3561858a80a1b063a41d123a3b2b5609e88
gaba1 <bruxblank@gmail.com>
2022-03-16 19:01:35 +0100

    log2 branch

--- algoritmus_tetelek_csharp/Program.cs
+++ algoritmus_tetelek_csharp/Program.cs
@@ -1,65 +1,33 @@
 ﻿using System;
 
-
-namespace Proj_02
+namespace feladat
 {
-    class Metszet
+    class Log2
     {
-        
         static void Main(string[] args)
         {
-            int[] A_tomb = new int[10] { 13, 28, 43, 37, 10, 5, 8, 99, 101, 2 };
-            int[] B_tomb = new int[5] { 6, 43, 37, 10, 88 };
-            int i, j, cdb = 0;
-            int[] C_tomb = new int[15];
+            int[] N = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int elso_szam = 0;
+            int utolso_szam = N.Length;
+            int keresett = 10;
+            int i = (elso_szam + utolso_szam) / 2;
 
-            Console.WriteLine("Az eredeti tömbök:");
+            Console.WriteLine("A keresett szám: {0}", keresett);
 
-            Console.WriteLine("A tömb: ");
-            for (i = 0; i < 10; i++)
-                {
-                    Console.Write("{0}\t", A_tomb[i]);
-                }
 
-            Console.WriteLine("\nB tömb:");
-            for (j = 0; j < 5; j++)
-                {
-                    Console.Write("{0}\t", B_tomb[j]);
-                }
-
-            // ===================================================================================================================================================================
-            // METSZETKÉPZÉS TÉTELE
-            // Rendelkezésre áll egy N> 0 és egy M > 0 elemű halmaz  az A[1..N] és B[1..M] vektorokban tárolva. Készítsük el a két halmaz metszetét a C[] vektorba!
-            //  Megoldás: vegyük egyenként az A[1..N] elemeit és nézzük meg, hogy találunk - e a B[1..M] elemei közt vele megegyezőt, s ha találunk tegyük ezt az elemet a C[]-be!
-
-            //    Bemenet: A[1..N], B[1..M]
-            //    Kimenet: C[]
-            //    Algoritmus :
-            //		Cdb:= 0
-            //        Ciklus i = 1 - től N - ig
-            //            j:= 1
-            //            Ciklus amíg j <= M és A[i] <> B[j]
-            //                j:= j + 1
-            //            Ciklus vége
-            //            Ha(j <= M) akkor Cdb:= Cdb + 1    :   C[Cdb]:= A[i]
-            //        Ciklus vége.
-            //    Algoritmus vége.
-
-            //metszet képzés
-            for (i = 0; i < 10; i++)
+            while (elso_szam <= utolso_szam)
             {
-                j = 0;
-                while (j < 5 && A_tomb[i] != B_tomb[j])
-                    j++;
-                if (j < 5) { C_tomb[cdb] = A_tomb[i]; cdb++; }
+                i = (elso_szam + utolso_szam) / 2;
+                if (keresett <= N[i])
+                {
+                    utolso_szam = i - 1;
+                }
+                else
+                {
+                    elso_szam = i + 1;
+                }
             }
-
-            // metszet kiírása
-            Console.WriteLine("\nA metszet tömb elemei:");
-            for (j = 0; j < cdb; j++)
-            {
-                Console.Write("{0}\t", C_tomb[j]);
-            }
+            Console.WriteLine("Indexe {0}", i);
 
             Console.ReadKey();
         }
